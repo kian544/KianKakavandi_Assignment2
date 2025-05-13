14c08f531e59c390750457247a1e9a57d019f853
kian544 <147429923+kian544@users.noreply.github.com>
2025-05-13 06:53:20 -0500

    Created Wizard and Warrior classes

---
+++ src/main/java/Warrior.java
@@ -0,0 +1,5 @@
+public class Warrior extends Character{
+    public Warrior(String name){
+        super(name, 5, 2,0);
+    }
+}
---
+++ src/main/java/Wizard.java
@@ -0,0 +1,5 @@
+public class Wizard extends Character{
+    public Wizard(String name){
+        super(name, 5, 0, 2);
+    }
+}
