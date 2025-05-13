86dec350c09c76868629b1b7d8569916f2b30a87
kian544 <147429923+kian544@users.noreply.github.com>
2025-05-13 06:19:08 -0500

    Fully copied over TextUI, ready to begin

--- src/main/java/TextUI.java
+++ src/main/java/TextUI.java
@@ -17,7 +17,27 @@ public class TextUI {
         StringBuilder s=new StringBuilder();
         s.append("You're in a chamber with" + r.getDoors().size()+"doors\n");
         s.append("There are"+ r.getItems().size()+"items in the chamber!\n");
+        //TODO: print for each door which monster is there, how strong, how skilled, and its health
+        System.out.println(s.toString());
+    }
+    //Asks user for action
+    private Action ask(Dungeon d){
+        StringBuilder s=new StringBuilder();
+        s.append("What do you want to do?");
+        List<Action> actions=d.getActions();
+        for(int i=0;i<actions.size();i++){
+            Action a=actions.get(i);
+            s.append("\t" + i + ": " + a.toString()+"\n");
+        }
+        System.out.println(s.toString());
 
-
+        //ask for action
+        BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+        try{
+            int command=Integer.parseInt(reader.readLine());
+            return actions.get(command);
+        } catch(IOException e){
+            return new PrintError(d, e);
+        }
     }
 }
