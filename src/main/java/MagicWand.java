d04ccc07d7dc46b0f0a77567fbcb473d590368e8
kian544 <147429923+kian544@users.noreply.github.com>
2025-05-13 06:50:53 -0500

    Updated to craft

--- src/main/java/MagicWand.java
+++ src/main/java/MagicWand.java
@@ -1,7 +1,7 @@
 public class MagicWand extends Item{
-    public int getHealth(){return 2;}
+    public int getHealth(){return 0;}
     public int getStrength(){return 0;}
-    public int getCraft(){return 0;}
+    public int getCraft(){return 2;}
     public String toString(){
         return "Magic Wand";
     }
